fe426b608c9749ec46b2d6524429cb83b6b61bf2
Elisa Shapiro <elisashapiro@gmail.com>
2021-09-22 14:55:28 -0400

    backend auth setup user model migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    #validations
+    validates :name, presence: true, uniqueness: :true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 end
