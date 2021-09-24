fdd5874ce62285147a30f5174f29f6b63774e276
Elisa Shapiro <elisashapiro@gmail.com>
2021-09-23 21:06:43 -0400

    datefns in

--- client/src/components/EventDetail.js
+++ client/src/components/EventDetail.js
@@ -1,10 +1,14 @@
+import { format } from 'date-fns';
+
 function EventDetail({ oneEvent }){
+
+
     return(
         <div>
             <p>Category: {oneEvent.category.category_name}</p>
             <p>What happened? {oneEvent.content}</p>
             <p>Severity: {oneEvent.severity}</p>
-            <p>Time: {oneEvent.event_time}</p>
+            <p>Occurred: {format(new Date(oneEvent.event_time), "eee MMM'-'d'-'y h':'mm bbb") }</p>
             
             
         </div>
